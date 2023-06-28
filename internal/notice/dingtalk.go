4969818f022ace0386a03d4035386c292185c78c
xxxuuu <x751736277@gmail.com>
2023-06-28 18:21:23 +0800

    fix: dingtalk invalid URL

--- internal/notice/dingtalk.go
+++ internal/notice/dingtalk.go
@@ -15,7 +15,7 @@ type Dingtalk struct {
 
 func (d Dingtalk) Notice(ctx app.EventContext, srv any) {
 	if sender, ok := srv.(DingtalkSender); ok {
-		content, token := sender.SendDingtalk(ctx)
+		token, content := sender.SendDingtalk(ctx)
 		resp, err := http.Post(fmt.Sprintf(DingtalkUrl, token), "application/json", bytes.NewBuffer([]byte(content)))
 		if err != nil {
 			log.Println(err)
